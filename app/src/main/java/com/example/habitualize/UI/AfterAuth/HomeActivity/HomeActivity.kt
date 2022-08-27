e4609f6260ffc07a376858e38dc7a61474ae384b
Ahmad-39-18 <ahmadshiekh2233@gmail.com>
2022-08-27 12:51:40 +0500

    working on bottom bar

---
+++ app/src/main/java/com/example/habitualize/UI/AfterAuth/HomeActivity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.habitualize.UI.AfterAuth.HomeActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.habitualize.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/habitualize/UI/AfterAuth/w.kt
@@ -0,0 +1,4 @@
+package com.example.habitualize.UI.AfterAuth
+
+class w {
+}
